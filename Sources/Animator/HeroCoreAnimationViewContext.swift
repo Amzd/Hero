dd25840b58a948581c9be65be0c3166589b36e5f
Joseph Mattiello <mail@joemattiello.com>
2021-02-07 19:58:17 -0500

    update for swift 5

--- Sources/Animator/HeroCoreAnimationViewContext.swift
+++ Sources/Animator/HeroCoreAnimationViewContext.swift
@@ -152,7 +152,7 @@ internal class HeroCoreAnimationViewContext: HeroAnimatorViewContext {
       anim = banim
     }
 
-    anim.fillMode = CAMediaTimingFillMode.both
+    anim.fillMode = convertToCAMediaTimingFillMode(CAMediaTimingFillMode.both.rawValue)
     anim.isRemovedOnCompletion = false
     anim.beginTime = beginTime
     return anim
@@ -459,6 +459,11 @@ internal class HeroCoreAnimationViewContext: HeroAnimatorViewContext {
     }
     return animate(delay: targetState.delay, duration: duration)
   }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertToCAMediaTimingFillMode(_ input: String) -> CAMediaTimingFillMode {
+	return CAMediaTimingFillMode(rawValue: input)
 }
 
 #endif
